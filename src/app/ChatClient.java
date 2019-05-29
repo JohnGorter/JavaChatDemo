7f84f409fdc6b87e0a6afa3ca1a3fed6c71a338f
john gorter <john.gorter@gmail.com>
2019-05-29 15:18:01 +0200

    final

--- src/app/ChatClient.java
+++ src/app/ChatClient.java
@@ -1,14 +1,17 @@
 package app;
 
 
-class ChatClient {
+class ChatClient implements IWantNewMessages {
 
     ChatRoom room;
     String name;
     IWantNewMessages newMessageProcessor;
+    WebSocketConnection connection;
 
     public void start(String name) {
-        room = new ChatRoom(); 
+        this.name = name;
+        connection = new WebSocketConnection(); 
+        connection.connect("ws://172.16.38.63:8085", this);
     }
 
     public void addMessageReceiveListener(IWantNewMessages listener){
@@ -17,10 +20,16 @@ class ChatClient {
 
     public void sendMessage(String kamer, String message){
         System.out.println("message to " + kamer + " send: " + message);
-        if (newMessageProcessor != null)
+        connection.sendMessage(name + " says: " + message);
+    }
+
+    @Override
+    public void processNewMessage(String message) {
+        if (newMessageProcessor != null) 
             newMessageProcessor.processNewMessage(message);
     }
 
+    
 }
 
 
--- src/app/WebSocketConnection.java
+++ src/app/WebSocketConnection.java
@@ -1,17 +1,27 @@
 package app;
 
 import java.net.http.*;
+import java.net.URI;
+import java.util.concurrent.*;
 
-class WebSocketServer {
+class WebSocketConnection implements WebSocket.Listener {
+    WebSocket server;
+    IWantNewMessages listener;
 
-    public Boolean connect(String url){
-        // HttpClient client = HttpClient.newHttpClient();
-        // CompletableFuture<WebSocket> ws = client.newWebSocketBuilder()
-        //         .buildAsync(URI.create(url), listener);
+    public Boolean connect(String url, IWantNewMessages l){
+        listener = l;
+        CompletableFuture<WebSocket> server_cf = HttpClient.newHttpClient().newWebSocketBuilder().buildAsync(URI.create(url),  this);
+        server = server_cf.join();
         return true;
     }
 
     public void sendMessage(String message){
-
+        server.sendText(message, true);
+    }
+    
+    @Override
+    public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+        listener.processNewMessage("" + data);
+        return WebSocket.Listener.super.onText(webSocket, data, last);
     }
 }
